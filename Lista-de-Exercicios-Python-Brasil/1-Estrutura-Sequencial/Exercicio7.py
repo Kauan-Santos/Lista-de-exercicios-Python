33551048d106f05f196780333478486c929769c2
Kauan-Santos <kasantos.business@gmail.com>
2022-10-04 17:09:20 -0300

    Adicionado o exercicio 7

---
+++ Lista-de-Exercicios-Python-Brasil/1-Estrutura-Sequencial/Exercicio7.py
@@ -0,0 +1,6 @@
+lado_a = float(input("Insira o lado A do quadrado: "))
+lado_b = float(input("Insira o lado B do quadrado: "))
+
+area_quadrado = lado_a * lado_b
+
+print(f"O dobro da área do quadrado é {area_quadrado * 2}")
