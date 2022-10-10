7aa82c115df1c72db559dc6b3248c9be5bbc1f23
Kauan-Santos <kasantos.business@gmail.com>
2022-10-10 16:18:04 -0300

    Adicionado exercício 6

---
+++ Lista-de-Exercicios-Python-Brasil/1-Estrutura-Sequencial/Exercicio10.py
@@ -0,0 +1,5 @@
+celsius = float(input("Insira a temperatura em graus Celsius a ser convertida: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"A temperatura em graus Celsius é: {fahrenheit:.1f}°")
