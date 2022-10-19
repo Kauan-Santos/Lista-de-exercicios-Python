0ed44d179cbdb17552f51334cb88be5c2b03de0b
Kauan-Santos <kasantos.business@gmail.com>
2022-10-19 16:50:10 -0300

    Adicionado exercicio 14

---
+++ Lista-de-Exercicios-Python-Brasil/1-Estrutura-Sequencial/Exercicio14.py
@@ -0,0 +1,7 @@
+peso_peixes = float(input("Insira o peso dos peixes: "))
+
+if peso_peixes >= 50:
+    excesso = peso_peixes - 50
+    multa = excesso * 4
+    print(f"O peso excedente foi: {excesso:.3f}kg")
+    print(f"O valor da multa é: R${multa:.2f}")
