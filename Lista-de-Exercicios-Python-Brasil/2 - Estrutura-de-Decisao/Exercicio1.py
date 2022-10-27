cb0885c2299f4545afac5571bc3f69250dcb0cd6
Kauan-Santos <kasantos.business@gmail.com>
2022-10-27 17:04:48 -0300

    Adicionado exercicio 1 - estrutura de decisão

---
+++ Lista-de-Exercicios-Python-Brasil/2 - Estrutura-de-Decisao/Exercicio1.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Insira um número: "))
+numero_2 = int(input("Insira outro número: "))
+
+if numero_1 > numero_2:
+    print(f"O maior número é: {numero_1}")
+else:
+    print(f"O maior número é: {numero_2}")
