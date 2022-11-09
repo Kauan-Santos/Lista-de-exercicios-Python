6015b659e2e62973669e97db2985916813a8d6f3
Kauan-Santos <kasantos.business@gmail.com>
2022-11-09 18:59:49 -0300

    Adicionado exercicio 06

---
+++ Lista-de-Exercicios-Python-Brasil/2-Estrutura-de-Decisao/Exercicio06.py
@@ -0,0 +1,7 @@
+numeros = []
+
+numeros.append(int(input("Insira o 1º número: ")))
+numeros.append(int(input("Insira o 2º número: ")))
+numeros.append(int(input("Insira o 3º número: ")))
+
+print(f"O maior número é: {max(numeros)}")
