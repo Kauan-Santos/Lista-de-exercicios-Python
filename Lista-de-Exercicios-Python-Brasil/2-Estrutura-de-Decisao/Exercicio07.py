0f5910683e4e1f98bde9b9a056cbcb722f119fdd
Kauan-Santos <kasantos.business@gmail.com>
2022-11-09 19:00:02 -0300

    Adicionado exercicio 07

---
+++ Lista-de-Exercicios-Python-Brasil/2-Estrutura-de-Decisao/Exercicio07.py
@@ -0,0 +1,8 @@
+numeros = []
+
+numeros.append(int(input("Insira o 1º número: ")))
+numeros.append(int(input("Insira o 2º número: ")))
+numeros.append(int(input("Insira o 3º número: ")))
+
+print(f"O maior número é: {max(numeros)}")
+print(f"O menor número é: {min(numeros)}")
