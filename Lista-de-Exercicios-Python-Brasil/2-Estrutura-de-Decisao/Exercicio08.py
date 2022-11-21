0a9c5a0901ca5e35283422612521f56353001813
Kauan-Santos <kasantos.business@gmail.com>
2022-11-21 17:02:11 -0300

    Adicionado exercicio 8

---
+++ Lista-de-Exercicios-Python-Brasil/2-Estrutura-de-Decisao/Exercicio08.py
@@ -0,0 +1,7 @@
+produto1 = float(input("Insira o preço do 1º produto: "))
+produto2 = float(input("Insira o preço do 2º produto: "))
+produto3 = float(input("Insira o preço do 3º produto: "))
+
+barato = min([produto1, produto2, produto3])
+
+print(barato)
